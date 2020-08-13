b5ea91a4b6575f12db1a9ccbe8f1d382d77674c0
samxkim <36899727+samxkim@users.noreply.github.com>
2020-08-12 22:06:09 -0700

    Updated dml to include coaches, players, and referees
     SELECT, INSERT, UPDATE, and DELETE

--- dml.sql
+++ dml.sql
@@ -92,3 +92,89 @@ order by game.gameID;
 
 --add referee to game
 insert into Games_Referees (gameID, refereeID) VALUES (:GAME_ID_FROM_PAGE, :REFEREE_ID_FROM_PAGE);
+
+--Coaches
+--Show coaches data
+SELECT coachID, firstName, lastName, phone, email, team.teamName 
+as 'Team' FROM Coaches LEFT JOIN Teams team on Coaches.teamID = team.teamID
+
+--Select teamid and teamname from Teams
+SELECT teamID, teamName FROM Teams
+
+--Verify phone as unique in coaches
+SELECT count(phone) FROM Coaches WHERE phone = :PHONE_FROM_PAGE
+
+--Verify email as unique in coaches
+SELECT count(email) FROM Coaches WHERE email = :EMAIL_FROM_PAGE
+
+--Add coach with NULL team
+INSERT INTO Coaches (firstName, lastName, phone, email, teamID) 
+VALUES (:FNAME_FROM_PAGE,:LNAME_FROM_PAGE,:PHONE_FROM_PAGE,:EMAIL_FROM_PAGE,NULL)
+
+--Add coach with certain team
+INSERT INTO Coaches (firstName, lastName, phone, email, teamID) 
+VALUES (:FNAME_FROM_PAGE,:LNAME_FROM_PAGE,:PHONE_FROM_PAGE,:EMAIL_FROM_PAGE,(SELECT teamID FROM Teams WHERE teamID = :TEAM_ID_FROM_PAGE))
+
+--Select coach info for update coach
+SELECT coachID, firstName, lastName, phone, email, team.teamName 
+as 'Team' FROM Coaches LEFT JOIN Teams team on Coaches.teamID = team.teamID 
+WHERE coachID = :COACHID_FROM_PAGE
+
+--Update coach with NULL team
+UPDATE Coaches SET firstName = :FNAME_FROM_PAGE, lastName = :LNAME_FROM_PAGE, phone = :PHONE_FROM_PAGE, email = :EMAIL_FROM_PAGE, teamID = NULL 
+WHERE coachID = :COACHID_FROM_PAGE
+
+--Update coach with certain team
+UPDATE Coaches SET firstName = :FNAME_FROM_PAGE, lastName = :LNAME_FROM_PAGE, phone = :PHONE_FROM_PAGE, email = :EMAIL_FROM_PAGE, teamID = :TEAM_ID_FROM_PAGE
+WHERE coachID = :COACHID_FROM_PAGE
+
+--Delete coach
+DELETE FROM Coaches WHERE coachID = :COACHID_FROM_PAGE
+
+--Players
+--Show player data
+SELECT playerID, firstName, lastName, phone, email, team.teamName as 'Team' 
+FROM Players LEFT JOIN Teams team on Players.teamID = team.teamID
+
+--Add player data with NULL team
+INSERT INTO Players (firstName, lastName, phone, email, teamID) 
+VALUES (:FNAME_FROM_PAGE,:LNAME_FROM_PAGE,:PHONE_FROM_PAGE,:EMAIL_FROM_PAGE,NULL)
+
+--Add player data with certain team
+INSERT INTO Players (firstName, lastName, phone, email, teamID) 
+VALUES (:FNAME_FROM_PAGE,:LNAME_FROM_PAGE,:PHONE_FROM_PAGE,:EMAIL_FROM_PAGE,(SELECT teamID FROM Teams WHERE teamID = :TEAM_ID_FROM_PAGE))
+
+--Select player info for update player
+SELECT playerID, firstName, lastName, phone, email, team.teamName as 'Team' 
+FROM Players LEFT JOIN Teams team on Players.teamID = team.teamID 
+WHERE playerID = :PLAYERID_FROM_PAGE
+
+--Update player with NULL team
+UPDATE Players SET firstName = :FNAME_FROM_PAGE, lastName = :LNAME_FROM_PAGE, phone = :PHONE_FROM_PAGE, email = :EMAIL_FROM_PAGE, teamID = NULL 
+WHERE playerID = :PLAYERID_FROM_PAGE
+
+--Update player with certain team
+UPDATE Players SET firstName = :FNAME_FROM_PAGE, lastName = :LNAME_FROM_PAGE, phone = :PHONE_FROM_PAGE, email = :EMAIL_FROM_PAGE, teamID = :TEAM_ID_FROM_PAGE 
+WHERE playerID = :PLAYERID_FROM_PAGE
+
+--Delete player
+DELETE FROM Players WHERE playerID = :PLAYERID_FROM_PAGE
+
+--Referees
+--Show referees on page
+SELECT refereeID, firstName, lastName, phone, email FROM Referees
+
+--Add referee
+INSERT INTO Referees (firstName, lastName, phone, email) 
+VALUES (:FNAME_FROM_PAGE,:LNAME_FROM_PAGE,:PHONE_FROM_PAGE,:EMAIL_FROM_PAGE)
+
+--Display referee info for update referee
+SELECT refereeID, firstName, lastName, phone, email 
+FROM Referees WHERE refereeID = :REFEREE_ID_FROM_PAGE
+
+--Update referee
+UPDATE Referees SET firstName = :FNAME_FROM_PAGE, lastName = :LNAME_FROM_PAGE, phone = :PHONE_FROM_PAGE, email = :EMAIL_FROM_PAGE 
+WHERE refereeID = :REFEREE_ID_FROM_PAGE
+
+--Delete referee
+SELECT firstName FROM Referees WHERE refereeID = :REFEREE_ID_FROM_PAGE
